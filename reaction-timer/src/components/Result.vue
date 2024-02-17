195e0c33c65af759ee9d73ba7e4b3b7c089a3451
ABDELLAHDIDI <93732229+ABDELLAHDIDI@users.noreply.github.com>
2024-02-17 19:00:40 +0100

    Add files via upload

---
+++ reaction-timer/src/components/Result.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="result" >
+    <h2>reaction time {{ score }} ms  </h2>
+    <h3>Rank : {{ rank }}</h3> 
+  </div>
+</template>
+
+<script>
+export default {
+props:['score' ] , 
+
+data(){
+    return{
+        rank : null 
+    }
+},
+mounted(){
+    if(this.score<100) this.rank="Speed of light "
+    else if(this.score<150) this.rank="Speed of sound "
+    else if(this.score<200) this.rank="Speed of horse "
+    else this.rank="Speed of human "
+}
+ 
+}
+</script>
+
+<style>
+ 
+</style>
